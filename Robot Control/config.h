74f82bbae71da9e19fbb36e16505a362a5753826
Chen Zhu <zhuchen115@gmail.com>
2017-01-22 16:34:03 +0100

    Removed Unused Files
    Signed-off-by: Chen Zhu <zhuchen115@gmail.com>

--- Robot Control/config.h
+++
@@ -1,6 +0,0 @@
-/// The spi channel of raspi
-#define SPI_CHANNEL 0
-/// According to the Arduino Speed, SPI speed is set to 500K
-#define SPI_CLOCK 500000
-
-#define NEED_ACK 1
--- ocv_slam/ocv_slam/ocv_inc.h
+++
@@ -1,7 +0,0 @@
-#ifndef OCV_INC_H
-#define OCV_INC_H
-
-#include <opencv2/opencv.hpp>
-#include <cmath>
-
-#endif
